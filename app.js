6e55dcf83cf89cf4a7a407fdf27cdc9851d580b1
Jan <jan.eberlein@student.fh-kiel.de>
2021-11-05 18:10:35 +0100

    fixes weeklyGoal calculation adds weeklyGoal method adds better description

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const Sandbox = {
 	data() {
 		return {
-			teamsize: 1,
+			teamsize: 3,
 			workdays: 5,
 			timeframe: 20
+		}
+	},
+	methods: {
+		weeklyGoal(currentWeek) {
+			const maxScore = this.teamsize * this.workdays * 2;
+			const difficulty = 5 + (4 / -this.timeframe * currentWeek);
+			return Math.floor(maxScore / difficulty);
 		}
 	}
 }
